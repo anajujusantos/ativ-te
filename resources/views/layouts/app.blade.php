12a57d5b297d41ab8c10d57fb4718fbbd46b770e
anajujusantos <anajujusantos0@gmail.com>
2019-06-18 16:38:20 -0300

    anachatajulia

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+@include('include.head')
+</head>
+<body>
+@include('include.header')
+
+<br>
+<div class="container">
+@yield('content')
+</div>
+
+<div class = "container - fluid">
+<footer class="row">
+@include('include.footer')
+</footer>
+</div>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
